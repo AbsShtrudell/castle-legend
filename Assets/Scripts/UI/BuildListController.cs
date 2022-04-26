d9bbc09b46fb0e03eb3ed8f9fb07b82fe581dcd3
Untermensch <70441070+AbsUntermensch@users.noreply.github.com>
2022-04-26 12:33:25 +0300

    ad

--- Assets/Scripts/UI/BuildListController.cs
+++ Assets/Scripts/UI/BuildListController.cs
@@ -6,23 +6,27 @@ using UnityEngine;
 public class BuildListController : MonoBehaviour
 {
     private RectTransform BuildListPanel;
+    private RectTransform BuildListButton;
     private bool listOpened = false;
 
     private void OnEnable()
     {
         BuildListPanel = transform.GetChild(0).GetComponent<RectTransform>();
+        BuildListButton = transform.GetChild(1).GetComponent<RectTransform>();
     }
 
     public void OnBuildListButtonClicked()
     {
         if (!listOpened)
         {
-            LeanTween.size(BuildListPanel, new Vector2(800, 180), Time.deltaTime * 10f);
+            LeanTween.size(BuildListPanel, new Vector2(800, 180), Time.deltaTime * 20f);
+            LeanTween.rotateZ(BuildListButton.gameObject, 180, Time.deltaTime * 20f);
             listOpened = true;
         }
         else
         {
-            LeanTween.size(BuildListPanel, new Vector2(0, 180),Time.deltaTime * 10f);
+            LeanTween.size(BuildListPanel, new Vector2(0, 180),Time.deltaTime * 20f);
+            LeanTween.rotateZ(BuildListButton.gameObject, 0, Time.deltaTime * 20f);
             listOpened = false;
         }
     }
